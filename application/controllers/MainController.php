903fd25e9fc8d86ee4f996cb1b2b4d55f5257515
baglansariev <baglansariev@mail.ru>
2019-07-23 17:46:34 +0600

    first commit

--- application/controllers/MainController.php
+++ application/controllers/MainController.php
@@ -11,6 +11,9 @@
 	    	$this->view->asset->setMetaDesc('Тестовое описание');
 	    	$this->view->asset->setMetaKeys('ключ1, ключ2');
 	    	$this->view->asset->setTitle('Главная');
+
+	    	// https://github.com/peachananr/purejs-onepage-scroll
+
 	    	$data['header'] = $this->load->controller('common/header');
 	    	$data['footer'] = $this->load->controller('common/footer');
 	    	$this->view->response('Main/index', $data);
